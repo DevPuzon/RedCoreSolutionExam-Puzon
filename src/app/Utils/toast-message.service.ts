3b0fe989d3be670efb5be1278695b58978419368
Michael Puzon <michaelpuzon06@gmail.com>
2020-07-08 16:02:24 +0800

    done

--- src/app/Utils/toast-message.service.ts
+++ src/app/Utils/toast-message.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastController } from '@ionic/angular';
-import { GoogleAuthService } from '../Authentication/AuthUtils/google-auth.service';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
